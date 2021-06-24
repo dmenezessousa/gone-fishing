80e274a0e232f5c5d4ed5ba1ac7b7bf4139b382d
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-06-24 10:37:08 -0400

    Final update

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 
 const prompt = require('prompt-sync')({ sigint: true });
 
+// list of Variables =======================================
+
 let gamerunnig = true;
 let time = 6;
 let fishcaught = [];
 let total = {
     fish: 0,
-    weigth: 0,
+    weight: 0,
     value: 0
 };
+
+// Fuctions ================================================
 
 function generateRandomFish(){
     let fishType = ['Big','Small','Deepsea','Slimy','Scaly','Bigmouthed','Longnose','Ugly','Pretty','Angry',];
@@ -21,44 +25,65 @@ function generateRandomFish(){
     return fishType[randomFishType] + ' ' + fishType2[randomFishType2] + ' ' + fishName[randomFishName];
 }
 
+//Greet User ================================================
+
 console.log("You've gone fishing! Try to maximize the value of your caught fish.");
 console.log('You can fish for six hours (till 12:00pm) and can catch at most 10 lbs of fish.');
 console.log();
 console.log('============================================');
 console.log();
 
+//Game Loop ==================================================
 while(gamerunnig){
+    //kill game when time >= the number 12.Time is set to 6.
+    //end game
     if(time >= 12){
-        gamerunnig = false;
+        gamerunnig = false; //End the game by changing (True)statment to false.
+
+        //Store all the info during the game and show it to the user at the end of it .
+
         console.log('The time is '+time+':00pm. Times up!');
         console.log();
         console.log('You caught ' + total.fish + ' fish:');
+
+        //For loop to display fish info after the time is up.
+
         for(i = 0; i < fishcaught.length; i++ ){
             console.log('* ' + fishcaught[i].Name + ', ' + fishcaught[i].Weight + ' lbs, $' + fishcaught[i].Value);
         }
         console.log();
-        console.log('Total weight: ' + total.weigth+' lbs');
+        console.log('Total weight: ' + total.weight+' lbs');
         console.log('Total value: $' + total.value);
     }
     else{
-        let randomWeigth = Math.floor(Math.random() * 1000) / 100;
+        //Variables to generate a random number for weights and Values.
+
+        let randomweight = Math.floor(Math.random() * 1000) / 100;
         let randomValue = Math.floor(Math.random() * 1000) / 100;
         let randomName = generateRandomFish();
+        
+        //Display user progress during the game.
+
         console.log("The time is "+ time +":00am. So far you've caught:");
-        console.log(total.fish + ' fish, ' + total.weigth.toFixed(2) + ' lbs, ' + '$' + total.value.toFixed(2));
+
+        //Values was showing all decimals needed to add toFixed(#) to display only 2 decimals
+
+        console.log(total.fish + ' fish, ' + total.weight.toFixed(2) + ' lbs, ' + '$' + total.value.toFixed(2)); 
         console.log();
-        console.log("You caught a "+ randomName+ " weighing "+randomWeigth+" lbs and valued at $"+ randomValue);
+        //generate randoms values for the obj total {name, weight , value} and log out.
+
+        console.log("You caught a "+ randomName+ " weighing "+randomweight+" lbs and valued at $"+ randomValue);
         console.log();
-        if((total.weigth + randomWeigth) < 10) {
+        if((total.weight + randomweight) < 10) {
             console.log('Your action: [c]atch or [r]elease?');
             const answer = prompt('>');
             console.log();
             if(answer === 'c'){
                 console.log('You chose to keep the fish.');
-                total.fish++;
-                total.weigth += randomWeigth;
+                total.fish++; // adds +1 to fish count.
+                total.weight += randomweight;
                 total.value += randomValue;
-                fishcaught.push({'Name': randomName, 'Weight': randomWeigth, 'Value': randomValue});
+                fishcaught.push({'Name': randomName, 'Weight': randomweight, 'Value': randomValue});// push fish info to the empty array.
             }else if(answer === 'r'){
                 console.log('You chose to release the fish.');
             }
@@ -70,5 +95,5 @@ while(gamerunnig){
         }      
     }
     console.log('==========================================');
-    time++; 
+    time++; //adds +1 to time.
 }
